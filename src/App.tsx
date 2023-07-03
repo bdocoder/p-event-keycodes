48e60dd25034a64376826e7d6aa09dc2ec8aff25
Abdullah Ahmad <bdocoder@protonmail.com>
2023-07-03 11:48:10 +0300

    feat: implement ui and logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,47 @@
+import { useLayoutEffect, useState } from "react";
+
 export default function App() {
-  return null;
+  const [event, setEvent] = useState<KeyboardEvent | null>(null);
+
+  useLayoutEffect(() => {
+    document.onkeydown = (ev) => {
+      ev.preventDefault();
+      setEvent(ev);
+    };
+  }, []);
+  return (
+    <div
+      className="has-background-light"
+      style={{ minHeight: "100vh", display: "flex" }}
+    >
+      <div className="columns" style={{ margin: "auto" }}>
+        {event ? (
+          <>
+            <div className="column has-text-centered">
+              <p className="block">event.key</p>
+              <div
+                className="box is-size-5"
+                style={{ minWidth: "10rem", minHeight: "70px" }}
+              >
+                {event.key}
+              </div>
+            </div>
+            <div className="column has-text-centered">
+              <p className="block">event.code</p>
+              <div
+                className="box is-size-5"
+                style={{ minWidth: "10rem", minHeight: "70px" }}
+              >
+                {event.code}
+              </div>
+            </div>
+          </>
+        ) : (
+          <div className="column">
+            <div className="box">Press any key to get the key code</div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
